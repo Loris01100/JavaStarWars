35ff5b8ca496cb4dd129c9508928a1c8cdedbdfc
Loris01100 <lolobach01100@gmail.com>
2025-11-06 14:08:22 +0100

    Initialisation

---
+++ fleet/src/main/java/org/ort/starwars/demo/fleetApplication.java
@@ -0,0 +1,13 @@
+package org.ort.starwars.fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class fleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(fleetApplication.class, args);
+	}
+
+}
